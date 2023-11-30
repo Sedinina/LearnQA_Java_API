d2a5a609efd3fbaac847986f58052f5ba38b4d01
Sedinina <sedinina.k@yandex.ru>
2023-11-30 22:16:14 +0500

    Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
   @Test
   public void testHelloWorld() {
-    System.out.println("Hello World");
+
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/hello")
+            .andReturn();
+    response.prettyPrint();
   }
 
   @Test
   public void testHelloName() {
     System.out.println("Hello from Ksenia");
   }
+
+  @Test
+  public void testGetText() {
+
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/get_text")
+            .andReturn();
+    response.prettyPrint();
+  }
+
+
+
+
 }
