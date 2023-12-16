9ae9f134e7dfb19460cd93e87abfab9a70afabba
Sedinina <sedinina.k@yandex.ru>
2023-12-16 11:15:25 +0500

    Ex12: Тест запроса на метод header

---
+++ src/test/java/HeaderTest.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HeaderTest {
+
+
+  @Test
+  public void cookieTest() {
+    Response response = RestAssured
+            .given()
+            .post("https://playground.learnqa.ru/api/homework_header")
+            .andReturn();
+
+    Headers headers = response.getHeaders();
+    assertTrue(headers.hasHeaderWithName("x-secret-homework-header"), "Response doesn't have 'x-secret-homework-header' header");
+
+    String valueSecretHeader = response.getHeader("x-secret-homework-header");
+    assertEquals("Some secret value", valueSecretHeader, "Response doesn't have header value 'Some secret value'");
+  }
+}
